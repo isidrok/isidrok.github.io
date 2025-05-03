551ca0588a9242b45a9430692ed79fd3f39b834f
Isidrok <isistorre95@gmail.com>
2025-05-03 16:43:51 +0200

    Update trailing slash configuration and blog post URLs for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://isidrok.com",
-  trailingSlash: "never",
+  trailingSlash: "always",
   integrations: [
     mdx(),
     sitemap({
