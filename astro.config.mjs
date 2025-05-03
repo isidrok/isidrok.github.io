af4bb8042af31d40d1229c354d93b687df1e1aa9
Isidrok <isistorre95@gmail.com>
2025-05-03 16:40:49 +0200

    Configure trailing slash behavior in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://isidrok.com",
+  trailingSlash: "never",
   integrations: [
     mdx(),
     sitemap({
