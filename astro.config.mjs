c638540092edaac0813a24a7e35a0fe9cbb9eadd
Isidrok <isistorre95@gmail.com>
2025-05-03 12:17:49 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,15 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://isidrok.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      filter: (page) => !page.includes("draft"),
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: "dark-plus",
